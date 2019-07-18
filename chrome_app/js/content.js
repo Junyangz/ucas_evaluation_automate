8c4a4c62b8493274d64f44a970fbf7877daba8d6
Junyangz <junyangz.iie@gmail.com>
2019-07-19 00:08:23 +0800

    Sync change with `evaluate-course.js`

--- chrome_app/js/content.js
+++ chrome_app/js/content.js
@@ -18,18 +18,15 @@ function evaluate(elements) {
 function main(id) {
 	evaluate(getElementsEndWithId(id));
 	var merit_rate = 5; //0-5
-	var merit = ""; //at least 15 characters
-	var flaw = "";
-	var suggest = "";
+	var merit = "课程特色鲜明，知识量适当，组织严密，概念清晰，逻辑性强。"; //at least 15 characters
+	var flaw = "希望增加互动环节，降低课程作业难度。";
+	var suggest = "课程内容选择是否合理，层次是否分明，知识点是否实用等。";
 	document.getElementById('rateit-range-2').setAttribute("aria-valuenow", 5);
 	document.getElementById('backing4').value = merit_rate;
 	document.getElementsByClassName('rateit-selected')[0].style = 'height: 32px; width: 160px; display: block;';
-	if (merit != "") document.getElementById('merit').value = merit;
-	else document.getElementById('merit').value = "课程特色鲜明，知识量适当，组织严密，概念清晰，逻辑性强。";
-	if (flaw != "") document.getElementById('flaw').value = flaw;
-	else document.getElementById('flaw').value = "希望增加互动环节，降低课程作业难度。";
-	if (suggest != "") document.getElementById('suggest').value = suggest;
-	else document.getElementById('suggest').value = "课程内容选择是否合理，层次是否分明，知识点是否实用等。";
+	document.getElementById('merit').value = merit;
+	document.getElementById('flaw').value = flaw;
+	document.getElementById('suggest').value = suggest;
 	document.getElementById("sb1").click();
 }
 
